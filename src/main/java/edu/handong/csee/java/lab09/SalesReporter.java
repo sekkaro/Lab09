c5cdc28e5ef7fd39848f4aee79853b09a8b3154d
outsider96 <junseoblee96@gmail.com>
2018-04-21 21:54:11 +0900

    implied arraylist and single line comments

--- src/main/java/edu/handong/csee/java/lab09/SalesReporter.java
+++ src/main/java/edu/handong/csee/java/lab09/SalesReporter.java
@@ -18,7 +18,7 @@ public class SalesReporter {
 	// private class variables declared
 	private double highestSales; // double variable to store highestSales
 	private double averageSales; // double variable to store value of average of sales
-	ArrayList<Salesman> team; // array of Salesman class called team that stores salesman instance in each of its method
+	ArrayList<Salesman> team; // ArrayList of Salesman class called team that stores salesman instance as index and have each of its method
 
 	/**
 	 * This is the main method.
@@ -43,7 +43,7 @@ public class SalesReporter {
 	 * This is a empty constructor of SalesReporter class.
 	 * <p>
 	 * This constructor basically asks the user to enter the number of salesman
-	 * to get the size of array team when the instance of the class is created.
+	 * to get the size of arraylist team when the instance of the class is created.
 	 */
 	// SalesReporter constructor
 	public SalesReporter() {
@@ -54,16 +54,16 @@ public class SalesReporter {
 	 * from the user.
 	 * <p>
 	 * This method guides the user to enter name and sales of each salesman and adds each of them 
-	 * to the array of salesman instances called team.
+	 * to the arraylist of salesman instances called team.
 	 */
 	// getData() method
 	public void getData() {
-		// defines the datatype of team array to be of Salesman class
-		team = new ArrayList<Salesman>(); // sets the length of team array to numOfSalesman
-		int i = 0;
+		// defines the datatype of team arraylist to be of Salesman class
+		team = new ArrayList<Salesman>(); // creates a instance of ArrayList of type Salesman class as team
+		int i = 0; // initializes to value of i as 0
 				
-		Scanner check = new Scanner(System.in);
-		// for loop to get to every index of team array
+		Scanner check = new Scanner(System.in); // defines scanner to check if the user wants to keep reading
+		// while loop to repeat until user types no and read salesmen as index of team 
 		while(true){
 			
 			System.out.println("Enter data for associate number " + (i+1)); // guides the user what Salesman number it is
@@ -79,14 +79,15 @@ public class SalesReporter {
 			Salesman mySalesman = new Salesman(); // while running the loop over each of the index of array, let each index be an instance of the Salesman class
 			mySalesman.setName(name); // uses setter method to set name for that particular team array element
 			mySalesman.setSales(sales); // uses setter method to set sales for that particular team array element
-			team.add(mySalesman);
-			i++;
+			team.add(mySalesman); // uses .add() method of arraylist team to keeping adding salesman instance to its index and this increases the size of team
+			i++; // increments variable i to keep check of how many salesman have been added
 			
-			System.out.print("Do you want to add data for more salesman? ");
-			String test = check.nextLine();
+			System.out.print("Do you want to add data for more salesman? "); // guides the user if they want to keep adding more salesmen
+			String test = check.nextLine(); // reads the user input as a line 
 			
+			// if the user types no (uses equalsIgnorecas3() method from string class to check without the cases)
 			if(test.equalsIgnoreCase("no")) {
-				break;
+				break; // then get out of the loop and stop reading the input from user
 			}
 		}
 
@@ -96,13 +97,13 @@ public class SalesReporter {
 	 * This is the calculateAverageSales() method.
 	 * <p>
 	 * Like the name says, this method calculates the average of the sales from each 
-	 * of the element of the team array that are instances of the Salesman class.
+	 * of the element of the team arraylist that are instances of the Salesman class.
 	 */
 	// calculateAverageSales() method
 	public void calculateAverageSales() {
 		double sum = 0; // initializes the sum as 0
 
-		// for loop to cover every element of the team array
+		// for loop to cover every element of the team arraylist
 		for(int i=0 ; i < team.size() ; i++) {
 			sum = sum + team.get(i).getSales(); // adds all the sales by using getter method where each elements of team array are instances of Salesman class
 		}
@@ -119,7 +120,7 @@ public class SalesReporter {
 	public void highestSales() {
 		highestSales = team.get(0).getSales(); // first sets the highestSales to be of the first element
 		
-		// uses for loop to get into each index of the team array
+		// uses for loop to get into each index of the team arraylist
 		for(int i=0 ; i < team.size() ; i++) {
 			// if the i+1(next element) does not give over bound exception and the current element's sales was less than the next element's sales, 
 			if(((i+1) != team.size()) && (team.get(i).getSales() < team.get(i+1).getSales())) {
@@ -141,7 +142,7 @@ public class SalesReporter {
 		System.out.println("Highest Sales: $" + highestSales + "\n"); // print out the highest sales that was calculated from method
 
 		System.out.println("The following had the highest Sales: "); // shows the details of the Salesman who had the highest sales
-		// Since we dont know the index who had the highest Sales we run a for loop to check the whole array
+		// Since we dont know the index who had the highest Sales we run a for loop to check the whole arraylist
 		for(int i = 0 ; i < team.size() ; i++) {
 			// if the sale of the element of a particular index at that point was equal to the value of highest sales stored,
 			if(team.get(i).getSales() == highestSales) {
@@ -154,7 +155,7 @@ public class SalesReporter {
 		System.out.println(); // prints out a \n to make it more nice and neat 
 
 		System.out.println("The rest performed as follows: "); // now it shows how the rest of the elements except for the ones with highest Sale.
-		// runs a for loop to check the whole array
+		// runs a for loop to check the whole arraylist
 		for(int i = 0 ; i < team.size() ; i++) {
 			// checks if the sales of that particular element is not the same as highest sales by using getter method
 			if(team.get(i).getSales() != highestSales) {
